1b717a46410fa2dedb3562695f7c2192dcf21557
saurabh <saurabhmate226@gmail.com>
2025-03-21 11:04:18 +0530

    update changes

--- main.tf
+++ main.tf
@@ -4,12 +4,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
   }
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
   region = "ap-south-1"
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
 
 resource "aws_instance" "app_server" {
@@ -81,6 +89,19 @@ resource "local_file" "inventory" {
   EOT
 }
 
+resource "cloudflare_record" "terraform_subdomain" {
+  zone_id = var.cloudflare_zone_id
+  name    = "saurabh"
+  value   = aws_instance.app_server.public_ip
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
+
 output "instance_public_ip" {
   value = aws_instance.app_server.public_ip
+}
+
+output "subdomain_url" {
+  value = "http://saurabh.purvesh.cloud"
 }
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+cloudflare_api_token = "bvnsRva_8NRSWljAIsZRfOmDL8-i-ND-3Ed9picZ"
+cloudflare_zone_id   = "67e5602a2ca4cdf80baf8c2233a89ed0"
